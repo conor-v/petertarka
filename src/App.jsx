7b2b4ef2476e40d5c1c0986f5a91bc3b861e9c65
conor vanoystaeyen <conor.vanoystaeyen@hotmail.com>
2022-11-26 21:08:40 +0100

    floor and wall setup

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,16 @@ function App() {
     <div className="App">
       <Canvas>
         <Suspense fallback={null}>
-          <mesh rotation-x={-Math.PI/2} position-y={-5}>
-            <planeGeometry args={[100, 100]}/>
-            <meshBasicMaterial color="red"/>
+          <mesh rotation-x={-Math.PI/2} position={[0, -16, 17]}>
+            <planeGeometry args={[145, 50]}/>
+            <meshBasicMaterial color="#D7D5D5"/>
           </mesh>
-          <mesh >
-            <boxGeometry />
+          <mesh position={[0, 9.5, -8]}>
+            <planeGeometry args={[145, 51]}/>
+            <meshBasicMaterial color="#CACACA"/>
+          </mesh>
+          <mesh position={[0, -11, 0]}>
+            <boxGeometry args={[10, 10, 10]}/>
             <meshBasicMaterial color="blue"/>
           </mesh>
         </Suspense>
@@ -23,7 +27,7 @@ function App() {
             near={1}
             far={1000}
             fov={40}
-            position={[0, 0, 50]}
+            position={[0, 5, 90]}
           />
           <OrbitControls />
       </Canvas>
